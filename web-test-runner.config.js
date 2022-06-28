159633d8fa67721536f6d63e491e8cc79eef5adf
Juan Muñoz <juan.munoz@alliander.com>
2022-06-28 08:27:41 +0200

    chore: added @web/test-runner-visual-regression

---
+++ web-test-runner.config.js
@@ -0,0 +1,9 @@
+import { visualRegressionPlugin } from '@web/test-runner-visual-regression/plugin';
+
+export default {
+  plugins: [
+    visualRegressionPlugin({
+      update: process.argv.includes('--update-visual-baseline'),
+    }),
+  ],
+};
