788ceeb06783d1a351258388a2505af35fb48bc1
Christian Dinkel <christian.dinkel@omicronenergy.com>
2022-11-10 14:25:10 +0100

    Updates

---
+++ foundation.js
@@ -0,0 +1,3 @@
+export { newOpenEvent } from './foundation/open-event.js';
+export { newEditEvent, isComplex, isInsert, isNamespaced, isUpdate, isRemove, } from './foundation/edit-event.js';
+//# sourceMappingURL=foundation.js.map
